1ad0150bcee3c3307d8c35523dbbb9de48fda9ba
Alech <alech@iastate.edu>
2017-06-03 14:43:51 -0500

    Made a tick tac toe game

---
+++ Tick Tac ToeTests/Tick_Tac_ToeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tick_Tac_ToeTests.swift
+//  Tick Tac ToeTests
+//
+//  Created by Alec Harrison on 5/29/17.
+//  Copyright © 2017 Meráki Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Tick_Tac_Toe
+
+class Tick_Tac_ToeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
